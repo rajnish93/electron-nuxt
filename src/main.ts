3ee2688126d94d487fe22e19afb8a2bfc1120746
Rajnish Singh <rajnish@reluminos.com>
2025-05-21 13:28:35 +0530

    fix: remove pnpm and added update app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
+import { updateElectronApp } from 'update-electron-app';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -21,7 +22,9 @@ const createWindow = () => {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
   // Open the DevTools.
@@ -49,6 +52,8 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+updateElectronApp();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
